796c87d8204cf5fa9d858b2de3aede7c8337e7b1
arosspope <andrew.pope456@gmail.com>
2017-11-08 11:13:47 +1100

    basic eliza program control flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("ELIZA begin.");
+    println!("  Enter '/quit' to close session.");
+    //eliza init -> loads eliza script (could use cmdline arg for script location)
+    //eliza print greeting
+
+    println!("Welcome!");
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("  Failed to read line.");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("  Failed to read line.");
+
+        match input.as_ref() {
+            "/quit\n" => break,
+            _ => println!("Go on..."),
+        }
+    }
+
+    println!("Lets resume next session.");
 }
