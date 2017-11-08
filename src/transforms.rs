c406c1bed212e4fc9fb2fd0a870526cc7cc3fb7b
arosspope <andrew.pope456@gmail.com>
2017-11-08 14:44:20 +1100

    fix transforms script

--- src/transforms.rs
+++ src/transforms.rs
@@ -19,6 +19,8 @@ impl Transforms {
     pub fn load(path: &str) -> Result<Transforms, Box<Error>> {
         let path = PathBuf::from(path).join("transforms.json");
 
+        println!("{:?}", path);
+
         let file = File::open(path.as_path())?;
         let u = serde_json::from_reader(file)?;
 
